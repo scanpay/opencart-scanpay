4814a8ee4fc859f765cadcbf1e715885e867202a
Ulrik Moe <ulrik@scanpay.dk>
2023-11-14 07:49:53 +0100

    Add more files to sed

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ read -p "Use test environment (api.scanpay.dev)? (y/N): " testing
 if [[ $testing =~ [yY] ]]; then
     sed -i 's/api\.scanpay\.dk/api\.scanpay\.dev/g' ".ocmod/upload/system/library/scanpay/client.php"
     sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/controller/extension/payment/scanpay.php"
+    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay.twig"
+    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay_order.twig"
 fi; echo
 
 cd ".ocmod/"
