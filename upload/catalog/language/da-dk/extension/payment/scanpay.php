765fedcc44f5a8e04f38b54fd78df4c1eb55b2e7
Ulrik Moe <ulrik.moe@gmail.com>
2023-11-16 14:09:00 +0100

    Update i18n

---
+++ upload/catalog/language/da-dk/extension/payment/scanpay.php
@@ -0,0 +1,4 @@
+<?php
+
+    $_['text_title'] = 'Betalingskort';
+    $_['error_failed'] = 'Der opstod en fejl i betalingsprocessen: ';
--- upload/catalog/language/en-gb/extension/payment/scanpay.php
+++ upload/catalog/language/en-gb/extension/payment/scanpay.php
@@ -1,4 +1,4 @@
 <?php
 
     $_['text_title'] = 'Credit Card / Debit Card';
-    $_['error_failed'] = 'Payment link failed with the following error: ';
+    $_['error_failed'] = 'Payment failed with the following error: ';
