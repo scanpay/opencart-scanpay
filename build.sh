6badb94185a6294c5a809e61dbc53c9509014384
Ulrik Moe <ulrik.moe@gmail.com>
2023-12-18 12:16:35 +0100

    Add file

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ read -rp "Use test environment (api.scanpay.dev)? (y/N): " testing
 if [[ $testing =~ [yY] ]]; then
     sed -i 's/api\.scanpay\.dk/api\.scanpay\.dev/g' ".ocmod/upload/system/library/scanpay/client.php"
     sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/controller/extension/payment/scanpay.php"
+    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay/settings.twig"
     sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay/settings_applepay.twig"
     sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay/settings_mobilepay.twig"
     sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/javascript/scanpay/order.js"
