5333cd78f44f7a2484b6dfe0e8f9924edbf677f2
Ulrik Moe <ulrik.moe@gmail.com>
2023-12-22 14:11:32 +0100

    Compress and minify JS files

--- build.sh
+++ build.sh
@@ -1,19 +1,40 @@
 #!/bin/bash
 set -e
 
+compress() {
+    echo -n "compressing $1..."
+    zopfli --i100 "$1"
+    brotli --best -w 0 -o "$1.br" "$1"
+    touch -r "$1" -c "$1".{br,gz}
+    [[ $(stat -c%s "$1.gz") -ge $(stat -c%s "$1.br") ]] || rm "$1.br"
+    printf '\033[0;32mOK\033[0m\n'
+}
+
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 mkdir -p "$DIR/.ocmod"
-rsync -av "$DIR/upload/" "$DIR/.ocmod/upload"
+rsync -rptgD --delete "$DIR/upload/" "$DIR/.ocmod/upload"
 
-read -rp "Use test environment (api.scanpay.dev)? (y/N): " testing
-if [[ $testing =~ [yY] ]]; then
+# Insert version number
+read -rp "Write the version number (x.y.z): " version
+
+if [ -z "$version" ]; then
+    version='x.y.z'
+    echo "No version number. Using '$version' and the test environment"
     sed -i 's/api\.scanpay\.dk/api\.scanpay\.dev/g' ".ocmod/upload/system/library/scanpay/client.php"
-    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/controller/extension/payment/scanpay.php"
-    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay/settings.twig"
-    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay/settings_applepay.twig"
-    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/template/extension/payment/scanpay/settings_mobilepay.twig"
-    sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' ".ocmod/upload/admin/view/javascript/scanpay/order.js"
-fi; echo
+    find .ocmod/ -type f -name "*.php" -exec sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' {} +
+    find .ocmod/ -type f -name "*.twig" -exec sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' {} +
+    find .ocmod/ -type f -name "*.js" -exec sed -i 's/dashboard\.scanpay\.dk/dashboard\.scanpay\.dev/g' {} +
+fi;
+
+# Replace EXTENSION_VERSION with $version
+find .ocmod/ -type f -name "*.php" -exec sed -i "s/EXTENSION_VERSION/$version/g" {} +
+find .ocmod/ -type f -name "*.js" -exec sed -i "s/EXTENSION_VERSION/$version/g" {} +
+
+# Minify JavaScript
+find -L .ocmod -iname '*.js' | while read f; do
+    node_modules/.bin/terser "$f" --mangle -o "$f"
+    compress "$f"
+done
 
 cd "$DIR/.ocmod/"
-zip -r "$DIR/opencart-scanpay-2.1.1-alpha6.ocmod.zip" "upload/"
+zip -r "$DIR/opencart-scanpay-$version.ocmod.zip" "upload/"
